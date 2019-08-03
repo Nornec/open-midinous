2297e33da6cf20718f093d0741221eba8647844b
James Ratliff <jaynornec@gmail.com>
2019-08-02 22:15:34 -0400

    Add files via upload

--- ui.rb
+++ ui.rb
@@ -1,3 +1,18 @@
+class GtkRadioButtonEx < Gtk::RadioButton
+	type_register
+	def initialize
+		super()
+	end
+	define_signal('keybinding-event',nil,nil,nil)
+end
+class GtkCanvas < Gtk::DrawingArea
+	type_register
+	def initialize
+		super()
+	end
+	define_signal('delete-selected-event',nil,nil,nil)
+end
+
 class UI_Elements
 	# Construct a Gtk::Builder instance and load our UI description
 	def build_ui
@@ -8,7 +23,7 @@ class UI_Elements
 		def midinous 
 			@builder.get_object("midinous")
 		end
-		
+		midinous.add_events("key-press-mask")
 		def canvas
 			@builder.get_object("canvas")
 		end
@@ -25,45 +40,80 @@ class UI_Elements
 			@builder.get_object("canvas_scroll_v")
 		end
 
-		def loper_1
-			@builder.get_object("loper_1")
+		def main_tool_1
+			@builder.get_object("main_tool_1")
 		end		
-		def loper_2
-			@builder.get_object("loper_2")
+		def main_tool_2
+			@builder.get_object("main_tool_2")
 		end		
-		def loper_3
-			@builder.get_object("loper_3")
+		def main_tool_3
+			@builder.get_object("main_tool_3")
 		end		
-		def loper_4
-			@builder.get_object("loper_4")
-		end	
-		
+		def main_tool_4
+			@builder.get_object("main_tool_4")
+		end
+		canvas.add_events("button-press-mask")
+		canvas.add_events("button-release-mask")
+		canvas.add_events("pointer-motion-mask")
+		#canvas.add_events(Gdk::EventMask::BUTTON_PRESS_MASK.nick)
 	end
 	
-	def grid_set(canvas_size)
-		canvas.signal_connect("draw") do |_widget, cr| #i think _widget means current widget.
-			gr_gray = [0.5,0.5,0.9,1.0]
-			bg_blck = [0.0,0.0,0.0,1.0]
-			# fill background with black
-			cr.set_source_rgba(bg_blck)
-			cr.paint
-			cr.set_source_rgba(gr_gray)
-			
-			# generate the grid
-			grid_size = 50
-			x = grid_size
-			while x < canvas_size do
-				y = grid_size
-				while y < canvas_size do
-					cr.circle(x,y,1)
-					cr.fill
-					y += grid_size
+	def grid_set(canvas_size,grid_size)
+		UI::canvas.signal_connect("draw") do |_widget,cr| #i think _widget means current widget.
+			#if @startup == true
+			#	@startup = false
+				gr_gray  = [0.3,0.5,0.9,1.0]
+				gr_dgray = [0.5,0.5,0.5,0.1]
+
+				bg_blck = [0.1,0.1,0.1,1.0]
+				# fill background with black
+				cr.set_source_rgba(bg_blck)
+				cr.paint
+				cr.set_source_rgba(gr_gray)
+				
+				# generate the grid with a center point
+				x = grid_size
+				while x < canvas_size
+					y = grid_size
+					while y < canvas_size
+=begin
+						if y == (canvas_size/2) && x == (canvas_size/2)
+							cr.circle(x,y,5)
+							cr.fill
+						else
+							cr.circle(x,y,1)
+							cr.fill
+						end
+=end
+						cr.circle(x,y,1)
+						cr.fill
+						y += grid_size
+					end
+					x += grid_size
 				end
-				x += grid_size
-			end
-		canvas_h_adj.set_value(canvas_size/3)			
-		canvas_v_adj.set_value(canvas_size/2)
-		end
+				# generate the connection points between grid points
+				cr.set_source_rgba(gr_dgray)
+				x = grid_size
+				division = 4
+				y = canvas_size - grid_size
+				while x < canvas_size
+					cr.move_to(x,grid_size)
+					cr.line_to(x,canvas_size-grid_size)
+					cr.set_line_width(1)
+					cr.stroke
+					x += grid_size*division
+				end
+				y = grid_size
+				x = canvas_size - grid_size
+				while y < canvas_size
+					cr.move_to(grid_size,y)
+					cr.line_to(canvas_size-grid_size,y)
+					cr.set_line_width(1)
+					cr.stroke
+					y += grid_size*division
+				end
+			#end
+		end #event handler end
 	end
 
 end
