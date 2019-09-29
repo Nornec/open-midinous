8b251666e86358f8341ebf2a73c6ad7828411559
James Ratliff <jaynornec@gmail.com>
2019-09-29 02:47:59 -0400

    Add files via upload

--- midinous.rb
+++ midinous.rb
@@ -1,12 +1,12 @@
 require "midi"
 require "gtk3"
-require_relative "init"
-require_relative "debug"
+require_relative "./bin/init"
+#require_relative "debug"
 
 Gtk.main
 
 #TODO
 # Add "transform" as a play mode? One point changes parms after playing? Could be any number of parameters, may require a dialog.
-# Set up save/load logic, should only have to save the Nouspoints array and current scale somehow
+# Set up save/load logic, remember canvas properties as well as points.
 # Add "logic view" and utilize a secondary path build function and button label "Build Logic"
 #   Add dialog for logic controls instead?
