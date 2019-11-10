dd0d7aeaef15aa71482966885bb64caa554996d6
James Ratliff <jaynornec@gmail.com>
2019-11-10 14:24:38 -0500

    Added licensing and copyright information

--- lib/midinous.rb
+++ lib/midinous.rb
@@ -1,7 +1,21 @@
+#   Copyright (C) 2019 James "Nornec" Ratliff
+#
+#   Midinous is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Midinous is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Midinous.  If not, see <https://www.gnu.org/licenses/>.
+
 require "unimidi"
 require "midi-eye"
 require "gtk3"
-require_relative "./bin/init"
-#require_relative "./bin/debug"
+require "midinous/init"
 
 Gtk.main
